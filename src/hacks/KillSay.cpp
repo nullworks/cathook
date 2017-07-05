97bf085d76cae4a140f0a5dca52ed12f430eb579
SylveonBottle <depressedeevee@gmail.com>
2017-07-05 12:41:26 -0500

    Update KillSay.cpp

--- src/hacks/KillSay.cpp
+++ src/hacks/KillSay.cpp
@@ -12,7 +12,7 @@
 namespace hacks { namespace shared { namespace killsay {
 
 static CatEnum killsay_enum({"NONE", "CUSTOM", "DEFAULT", "NCC - OFFENSIVE", "NCC - MLG"});
-static CatVar killsay_mode(killsay_enum, "killsay", "0", "Killsay", "Defines source of killsay lines. CUSTOM killsay file must be set in cat_killsay_file and loaded with cat_killsay_reload (Use console!)");
+static CatVar killsay_mode(killsay_enum, "killsay", "0", "Killsay", "Defines source of killsay lines.");
 static CatVar filename(CV_STRING, "killsay_file", "killsays.txt", "Killsay file", "Killsay file name. Should be located in cathook data folder");
 static CatCommand reload("killsay_reload", "Reload killsays", Reload);
 

105e7e9d21dc714ce8cfa4421dca3ec95b314f84
oneechanhax <julianrowe9999@gmail.com>
2018-03-21 18:20:16 -0500

    Ipc finalizing

--- src/framework/ipc.cpp
+++ src/framework/ipc.cpp
@@ -5,8 +5,6 @@
  *
  */
 
-#include <thread> // Threads are useful!
-#include <vector>
 //#include <queue> // todo
 #ifdef __linux__
   #include <fcntl.h> // flags for shm_open()
@@ -16,212 +14,144 @@
   #include <sys/mman.h> // mmap
 #endif
 
-#include "../util/chrono.hpp" // for the cat timer, and so we can sleep in thread
-#include "../util/functions.hpp" // ThreadedLoop
 #include "../util/logging.hpp" // Logging is good u noob
+
+#include "ipc.hpp"
 
 // TODO: clean this shit up, it looks horrible!
 // TODO: Windows support?
 
-// Increase as needed, we just need enough space to handle
-#define MAX_IPC_MEMBERS 32
-#define MAX_IPC_MESSAGE_CAP (MAX_IPC_MEMBERS * MAX_IPC_MEMBERS / 8)
-
 namespace ipc {
 
-// IPC Memory space
+// Constructor responsibilities, constructing the shared memory pool and starting the thread if successful
+IpcStream::IpcStream(const char* _pool_name) : pool_name(_pool_name) {
+  #ifdef __linux__ // Linux only sadly, gotta find a way to do this in windows
+  // Try to get access to the memory pool
+  int shm_res = shm_open(this->pool_name, O_RDWR, S_IRWXU | S_IRWXG | S_IRWXO);
+  bool first_peer = false;
+  if (shm_res == -1) {
+    if (errno == ENOENT) { // The memory isnt mapped, we will do so ourselves.
+      shm_res = shm_open(this->pool_name, O_RDWR | O_CREAT, S_IRWXU | S_IRWXG | S_IRWXO);
+      first_peer = true;
+    }
+    if (shm_res == -1) { // check if we still have error from above, or above wasnt run
+      char buffer[1024];
+      const char* error_string = strerror_r(errno, buffer, sizeof(buffer));
+      g_CatLogging.log("IPC: Fatal shm_open error: %s", error_string);
+    }
+  }
+  if (shm_res == -1) {
+    g_CatLogging.log("IPC: Fatal shm_open error, panicing and stopping!");
+    return;
+  }
 
-enum ipc_state {
-  OPEN, // Item is open to the network
-  LOCKED, // Item is locked
-  RECIPIENT_LOCKED // Item is open to recipients
-};
-struct IpcSlot { // A slot, open, closed, simple as that
-  int state = ipc_state::OPEN;
-  CatTimer time;
-};
-struct IpcMessage : public IpcSlot { // A message, it is the recipients job to recieve it in time and reset it
-  int author; // number in array
-  int recipient;
-  char command[64]; // A command that tells the recipient what to do with the payload
-  char payload[1024]; // This could be anything
-};
-struct IpcMember : public IpcSlot {
-  char name[64] = "";
-};
-// This is what the entire Ipc mapped memory should be
-struct IpcContent {
-  IpcMember members[MAX_IPC_MEMBERS];
-  IpcMessage message_pool[MAX_IPC_MESSAGE_CAP];
-};
+  // Map the memory pool
+  this->IpcMemSpace = (IpcContent*)mmap(NULL, sizeof(IpcContent), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_32BIT, shm_res, 0);
+  if (this->IpcMemSpace == MAP_FAILED) {
+    g_CatLogging.log("IPC: Fatal mmap error, panicing and stopping!");
+    return;
+  }
 
-// IPC Driver
+  // If we are the first peers, we must setup everything ourselves
+  if (first_peer) {
+    g_CatLogging.log("IPC: Setting up ipc space!");
+    memset(this->IpcMemSpace, 0, sizeof(IpcContent));
+    *this->IpcMemSpace = IpcContent();
+  }
 
-// Ipc stream class
-class IpcStream {
-private:
-  // Constructor/Descructor needed stuff
-  const char* pool_name;
-  // Ipc stuff
-  IpcContent* IpcMemSpace = nullptr; // A pointer to the ipc space
-  int ipc_pos = -1; // our position in the ipc
-
-  // Setup of shared memory
-
-public:
-  IpcStream(const char* _pool_name) : pool_name(_pool_name) {
-    #ifdef __linux__ // Linux only sadly, gotta find a way to do this in windows
-    // Try to get access to the memory pool
-    int shm_res = shm_open(this->pool_name, O_RDWR, S_IRWXU | S_IRWXG | S_IRWXO);
-    bool first_peer = false;
-    if (shm_res == -1) {
-      if (errno == ENOENT) { // The memory isnt mapped, we will do so ourselves.
-        shm_res = shm_open(this->pool_name, O_RDWR | O_CREAT, S_IRWXU | S_IRWXG | S_IRWXO);
-        first_peer = true;
-      }
-      if (shm_res == -1) { // check if we still have error from above, or above wasnt run
-        char buffer[1024];
-        const char* error_string = strerror_r(errno, buffer, sizeof(buffer));
-        g_CatLogging.log("IPC: Fatal shm_open error: %s", error_string);
+  // Setup a member slot for us
+  this->IpcCleanup(); // cleanup to make sure we get unused slots
+  this->ipc_pos = GetOpenSlot(this->IpcMemSpace->members, MAX_IPC_MEMBERS);
+  if (this->ipc_pos != -1) {
+    // Set ping so we dont get instantly rejected by peers, then set the state to locked so we can setup
+    this->IpcMemSpace->members[this->ipc_pos].time.Reset();
+    this->IpcMemSpace->members[this->ipc_pos].state = ipc_state::LOCKED;
+    for (int i = 0; i < MAX_IPC_MEMBERS; i++) {
+      std::string test = "neko_" + std::to_string(i);
+      if (!MemberExists(test)) {
+        strcpy(this->IpcMemSpace->members[this->ipc_pos].name, test.c_str());
+        break;
       }
     }
-    if (shm_res == -1) {
-      g_CatLogging.log("IPC: Fatal shm_open error, panicing and stopping!");
-      return;
-    }
-
-    // Map the memory pool
-    this->IpcMemSpace = (IpcContent*)mmap(NULL, sizeof(IpcContent), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_32BIT, shm_res, 0);
-    if (this->IpcMemSpace == MAP_FAILED) {
-      g_CatLogging.log("IPC: Fatal mmap error, panicing and stopping!");
-      return;
-    }
-
-    // If we are the first peers, we must setup everything ourselves
-    if (first_peer) {
-      g_CatLogging.log("IPC: Setting up ipc space!");
-      memset(this->IpcMemSpace, 0, sizeof(IpcContent));
-      *this->IpcMemSpace = IpcContent();
-    }
-
-    // Setup a member slot for us
-    this->ipc_pos = GetOpenSlot(this->IpcMemSpace->members, MAX_IPC_MEMBERS);
-    if (this->ipc_pos != -1) {
-      // Set ping so we dont get instantly rejected by peers, then set the state to locked so we can setup
-      this->IpcMemSpace->members[this->ipc_pos].time.Reset();
-      this->IpcMemSpace->members[this->ipc_pos].state = ipc_state::LOCKED;
-      for (int i = 0; i < MAX_IPC_MEMBERS; i++) {
-        std::string test = "neko_" + std::to_string(i);
-        if (!MemberExists(test)) {
-          strcpy(this->IpcMemSpace->members[this->ipc_pos].name, test.c_str());
-          break;
-        }
-      }
-      if (strlen(this->IpcMemSpace->members[this->ipc_pos].name) != 0){
-        g_CatLogging.log("IPC: Found name: %s!", this->IpcMemSpace->members[this->ipc_pos].name);
-      } else {
-        g_CatLogging.log("IPC: Cant find suitible name!");
-        return;
-      }
+    if (strlen(this->IpcMemSpace->members[this->ipc_pos].name) != 0){
+      g_CatLogging.log("IPC: Found name: %s!", this->IpcMemSpace->members[this->ipc_pos].name);
     } else {
-      g_CatLogging.log("IPC: No open slot to register in!");
+      g_CatLogging.log("IPC: Cant find suitible name!");
       return;
     }
-
-    // Set us up with a slot and name
-    // Start a thread for the ipc
-    g_CatLogging.log("IPC: Starting Thread");
-    std::thread ipc_thread(&IpcStream::thread_loop, this);
-    ipc_thread.detach();
-    #else
-      #pragma message ("IPC DISABLED")
-    #endif
+  } else {
+    g_CatLogging.log("IPC: No open slot to register in!");
+    return;
   }
 
-  // Setters/Getters
+  // Set us up with a slot and name
+  // Start a thread for the ipc
+  g_CatLogging.log("IPC: Starting Thread");
+  std::thread ipc_thread(&IpcStream::thread_loop, this);
+  ipc_thread.detach();
+  #else
+    #pragma message ("IPC DISABLED")
+  #endif
+}
 
-public:
-  std::vector<std::string> GetMembers() {
-    std::vector<std::string> ret;
-    if (!this->IpcMemSpace)// Saftey net
-      return ret;
-    for (auto i: this->IpcMemSpace->members)
-      ret.push_back(i.name);
-    return ret;
-  }
-  std::string GetIpcName() {
-    if (!this->IpcMemSpace || ipc_pos == -1) return std::string();
-    return this->IpcMemSpace->members[ipc_pos].name;
-  }
-  bool MemberExists(const std::string& test_name) {
-    for (auto i: this->IpcMemSpace->members)
-      if (i.name == test_name)
-        return true;
-    return false;
-  }
-private:
-  // Internal cleanup of the ipc space, in the case of a member not being nice >:(
-  void IpcCleanup(){
-    // For the cleanup, we look for anything not open and check the timer on it
-    // We clean thing more than 3 seconds of idle
-    for (auto i : this->IpcMemSpace->members)
-      if (i.state != ipc_state::OPEN && i.time.CheckTime(std::chrono::seconds(3)))
-        i.state = ipc_state::OPEN;
-    for (auto i : this->IpcMemSpace->message_pool)
-      if (i.state != ipc_state::OPEN && i.time.CheckTime(std::chrono::seconds(3)))
-        i.state = ipc_state::OPEN;
-  }
-  // Internal use for finding open slots, be sure to give it the right size to prevent segfaults
-  inline int GetOpenSlot(IpcSlot* slot_array, int size){
-    for (int i = 0; i < size; i++)
-      if (slot_array[i].state == ipc_state::OPEN)
-        return i;
-    return -1;
-  }
+void IpcStream::thread_loop() {
 
-  // Threading, TODO: replace with ThreadedLoop
+  // Main loop
+  this->state = RUNNING;
+  while (this->state != STOP) {
 
-private:
-  enum {RUNNING, STOP, HALTED};
-  int state = HALTED;
-  CatTimer cleanup;
-  void thread_loop() {
+    // Ping keeper
+    this->IpcMemSpace->members[this->ipc_pos].time.Reset();
 
-    // Main loop
-    this->state = RUNNING;
-    while (this->state != STOP) {
-
-      // Ping keeper
-      this->IpcMemSpace->members[this->ipc_pos].time.Reset();
-
-      // Find new messages sent to us
-      for (auto i : this->IpcMemSpace->message_pool) {
-        if (i.state == ipc_state::RECIPIENT_LOCKED) {
-          if (i.recipient == this->ipc_pos) {
-            // TODO, make system to handle this
-          }
+    // Find new messages sent to us
+    for (auto i : this->IpcMemSpace->message_pool) {
+      if (i.state == ipc_state::RECIPIENT_LOCKED) {
+        if (i.recipient == this->ipc_pos) {
+          // TODO, make system to handle this
         }
       }
-
-      // Cleanup Crew, we clean every 15 seconds... why, i dont know /shrug
-      if (cleanup.ResetAfter(std::chrono::seconds(15)))
-        this->IpcCleanup();
-
-      // Loop sleep
-      std::this_thread::sleep_for(std::chrono::milliseconds(75));
     }
-    g_CatLogging.log("IPC: Thread recieved shutdown!");
-    this->state = HALTED;
+
+    // Cleanup Crew, we clean every 15 seconds... why, i dont know /shrug
+    if (cleanup.ResetAfter(std::chrono::seconds(15)))
+      this->IpcCleanup();
+
+    // Loop sleep
+    std::this_thread::sleep_for(std::chrono::milliseconds(75));
   }
-public:
-  ~IpcStream() {
-    g_CatLogging.log("IPC: IPC shutting down!");
-    this->state = STOP;
-    // We hang the thread deconstructing this until the loop thread ends to prevent segfaulting
-    while(this->state != HALTED)
-      std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    shm_unlink(this->pool_name);
-  }
-};
+  g_CatLogging.log("IPC: Thread recieved shutdown!");
+  this->state = HALTED;
+}
+
+IpcStream::~IpcStream() {
+  g_CatLogging.log("IPC: IPC shutting down!");
+  this->state = STOP;
+  // We hang the thread deconstructing this until the loop thread ends to prevent segfaulting
+  while(this->state != HALTED)
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  shm_unlink(this->pool_name);
+}
+
+bool IpcStream::SendMessage(std::string recipient, const char* command, const void* payload, size_t size) {
+  auto tmp = this->FindMember(recipient);
+  if (tmp == -1) return false;
+  this->SendMessage(tmp, command, payload, size);
+  return true;
+}
+bool IpcStream::SendMessage(int recipient, const char* command, const void* payload, size_t size) {
+  auto tmp = this->GetOpenSlot(this->IpcMemSpace->message_pool, MAX_IPC_MESSAGE_CAP);
+  if (tmp == -1) return false;
+  // Reset ping and lock so we can use without issues
+  this->IpcMemSpace->message_pool[tmp].time.Reset();
+  this->IpcMemSpace->message_pool[tmp].state = ipc_state::LOCKED;
+  // Address it
+  this->IpcMemSpace->message_pool[tmp].author = this->ipc_pos;
+  this->IpcMemSpace->message_pool[tmp].recipient = recipient;
+  // Copy our payload into the message
+  strcpy(this->IpcMemSpace->message_pool[tmp].command, command);
+  memcpy(this->IpcMemSpace->message_pool[tmp].payload, payload, size);
+  // Allow the recipient to read it
+  this->IpcMemSpace->message_pool[tmp].state = ipc_state::RECIPIENT_LOCKED;
+}
 
 }
--- src/framework/ipc.hpp
+++ src/framework/ipc.hpp
@@ -1,2 +1,118 @@
 
-void InitIpc();
+#include <thread> // Threads are useful!
+#include <vector>
+
+#include "../util/chrono.hpp" // for the cat timer, and so we can sleep in thread
+
+// Increase as needed, we just need enough space to handle
+#define MAX_IPC_MEMBERS 32
+#define MAX_IPC_MESSAGE_CAP (MAX_IPC_MEMBERS * MAX_IPC_MEMBERS / 8)
+
+namespace ipc {
+
+// IPC Memory space
+
+// State of a slot
+enum ipc_state {
+  OPEN, // Item is open to the network
+  LOCKED, // Item is locked
+  RECIPIENT_LOCKED // Item is open to recipients
+};
+// A slot, open, closed, simple as that
+struct IpcSlot {
+  int state = ipc_state::OPEN;
+  CatTimer time;
+};
+// A message, it is the recipients job to recieve it in time and reset it
+struct IpcMessage : public IpcSlot {
+  int author; // number in array
+  int recipient;
+  char command[64]; // A command that tells the recipient what to do with the payload
+  char payload[1024]; // This could be anything
+};
+struct IpcMember : public IpcSlot {
+  char name[64] = "";
+};
+// This is what the entire Ipc mapped memory should be
+struct IpcContent {
+  IpcMember members[MAX_IPC_MEMBERS];
+  IpcMessage message_pool[MAX_IPC_MESSAGE_CAP];
+};
+
+// IPC Driver
+
+// Ipc stream class
+class IpcStream {
+// Constructor/Descructor needed stuff
+private:
+  const char* pool_name;
+  // Ipc stuff
+  IpcContent* IpcMemSpace = nullptr; // A pointer to the ipc space
+  int ipc_pos = -1; // our position in the ipc
+
+// Setup of shared memory, Destructor, stops thread and unlinks shared memory
+public:
+  IpcStream(const char* _pool_name);
+  ~IpcStream();
+  
+// Setters/Getters
+public:
+  // Used to get a list of members
+  inline std::vector<std::string> GetMembers() {
+    std::vector<std::string> ret;
+    if (!this->IpcMemSpace)// Saftey net
+      return ret;
+    for (auto i: this->IpcMemSpace->members)
+      ret.push_back(i.name);
+    return ret;
+  }
+  // Returns the name used in ipc
+  inline std::string GetIpcName() {
+    if (!this->IpcMemSpace || ipc_pos == -1) return std::string();
+    return this->IpcMemSpace->members[ipc_pos].name;
+  }
+  // Returns whether member exists
+  inline bool MemberExists(const std::string& test_name) {
+    return (FindMember(test_name) == -1) ? false : true;
+  }
+  // Returns the slot of the member, returns -1 on failure
+  inline int FindMember(const std::string& test_name) {
+    for (int i = 0; i < MAX_IPC_MEMBERS; i++)
+      if (this->IpcMemSpace->members[i].name == test_name)
+        return i;
+    return -1;
+  }
+private:
+  // Internal cleanup of the ipc space, in the case of a member not being nice >:(
+  inline void IpcCleanup(){
+    // For the cleanup, we look for anything not open and check the timer on it
+    // We clean thing more than 3 seconds of idle
+    for (auto i : this->IpcMemSpace->members)
+      if (i.state != ipc_state::OPEN && i.time.CheckTime(std::chrono::seconds(3)))
+        i.state = ipc_state::OPEN;
+    for (auto i : this->IpcMemSpace->message_pool)
+      if (i.state != ipc_state::OPEN && i.time.CheckTime(std::chrono::seconds(3)))
+        i.state = ipc_state::OPEN;
+  }
+  // Internal use for finding open slots, be sure to give it the right size to prevent segfaults
+  inline int GetOpenSlot(IpcSlot* slot_array, int size){
+    for (int i = 0; i < size; i++)
+      if (slot_array[i].state == ipc_state::OPEN)
+        return i;
+    return -1;
+  }
+
+// Threading, TODO: replace with ThreadedLoop
+private:
+  enum {RUNNING, STOP, HALTED};
+  int state = HALTED;
+  CatTimer cleanup;
+  void thread_loop();
+public:
+  // Use to send messages through ipc, use member pos or by name, returns true on success
+  bool SendMessage(std::string recipient, const char* command, const void* payload, size_t size);
+  bool SendMessage(int recipient, const char* command, const void* payload, size_t size);
+
+};
+
+}

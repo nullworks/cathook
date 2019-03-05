c4b6ac21bb255aa0637150b196d91bee3c9b215c
LightCat <BenCat07@gmail.com>
2019-03-05 06:12:50 +0000

    Update Misc.cpp

--- src/hacks/Misc.cpp
+++ src/hacks/Misc.cpp
@@ -413,7 +413,7 @@ void Schema_Reload()
 
     fclose(file);
     logging::Info("Loading item schema...");
-    bool ret = BInitTextBuffer(schema, buf, 0xDEADCAT);
+    bool ret = BInitTextBuffer(schema, buf, (int)0xDEADCAT);
     logging::Info("Loading %s", ret ? "Successful" : "Unsuccessful");
 
     delete[] text_buffer;

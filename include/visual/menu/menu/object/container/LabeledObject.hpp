99d48f9689a3e15c8a5c6534cd17593977de2ee2
LightPower392 <60998601+LightPower392@users.noreply.github.com>
2020-03-06 23:01:50 -0800

    Updated Tooltip

--- include/visual/menu/menu/object/container/LabeledObject.hpp
+++ include/visual/menu/menu/object/container/LabeledObject.hpp
@@ -27,7 +27,7 @@ public:
 
     void setObject(std::unique_ptr<BaseMenuObject> &&object);
 
-    void setLabel(std::string text);
+    void setLabel(std::string text, std::string text2);
 
     void createLabel();
 

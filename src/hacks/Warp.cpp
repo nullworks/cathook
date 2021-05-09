8c4c4ec9971f7beeddde8cfab9dc50d7ea298b71
Lewdcifer <83784372+Lewdcifer@users.noreply.github.com>
2021-05-09 22:41:31 +0800

    Update Warp.cpp

--- src/hacks/Warp.cpp
+++ src/hacks/Warp.cpp
@@ -198,7 +198,7 @@ bool shouldRapidfire()
     if (CE_BAD(LOCAL_E) || !LOCAL_E->m_bAlivePlayer() || CE_BAD(LOCAL_W))
         return false;
 
-    // Weapon specific ignores, knives, Huntsman, Mediguns, and the grappling hook
+    // Weapon specific ignores, knives, Thermal Thruster, Huntsman, Mediguns, and the grappling hook
     if (re::C_TFWeaponBase::GetWeaponID(RAW_ENT(LOCAL_W)) == 7 || LOCAL_W->m_iClassID() == CL_CLASS(CTFRocketPack) || LOCAL_W->m_iClassID() == CL_CLASS(CTFCompoundBow) || LOCAL_W->m_iClassID() == CL_CLASS(CWeaponMedigun) || LOCAL_W->m_iClassID() == CL_CLASS(CTFGrapplingHook))
         return false;
 

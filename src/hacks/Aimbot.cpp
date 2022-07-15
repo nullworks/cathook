0b9a8522134f682634ad7197bdcf853073824955
Stephen Martin <stephenmartinemail2003@gmail.com>
2022-07-14 20:14:29 -0400

    Wow, pretty big bugfix. Multipoint works now!

--- src/hacks/Aimbot.cpp
+++ src/hacks/Aimbot.cpp
@@ -142,7 +142,8 @@ std::vector<Vector> getValidHitpoints(CachedEntity *ent, int hitbox)
     auto hb = ent->hitboxes.GetHitbox(hitbox);
 
     trace_t trace;
-    if (IsEntityVectorVisible(ent, hb->center, true, MASK_SHOT_HULL, &trace))
+    trace_t *new_trace_pointer = &trace;
+    if (IsEntityVectorVisible(ent, hb->center, true, MASK_SHOT_HULL, new_trace_pointer))
     {
         if (trace.hitbox == hitbox)
             hitpoints.push_back(hb->center);
@@ -970,16 +971,18 @@ bool IsTargetStateGood(CachedEntity *entity)
             {
                 int i = 0;
                 trace_t first_tracer;
-                if (IsEntityVectorVisible(entity, entity->hitboxes.GetHitbox(cd.hitbox)->center, true, MASK_SHOT_HULL, &first_tracer))
+                trace_t *send_tracer = &first_tracer;
+                if (IsEntityVectorVisible(entity, entity->hitboxes.GetHitbox(cd.hitbox)->center, true, MASK_SHOT_HULL, send_tracer))
                     return true;
                 while (i <= 17) // Prevents returning empty at all costs. Loops through every hitbox
                 {
                     if (i == cd.hitbox && i != 17)
                         i++;
                     trace_t test_trace;
+                    trace_t *new_trace_pointer          = &first_tracer;
                     std::vector<Vector> centered_hitbox = getHitpointsVischeck(entity, i);
 
-                    if (IsEntityVectorVisible(entity, centered_hitbox[0], true, MASK_SHOT_HULL, &test_trace))
+                    if (IsEntityVectorVisible(entity, centered_hitbox[0], true, MASK_SHOT_HULL, new_trace_pointer))
                     {
                         cd.hitbox = i;
                         return true;
